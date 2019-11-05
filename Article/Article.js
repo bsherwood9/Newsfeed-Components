8bbef45ba73bff1f2e2d2514bb983096bf5ae02a
bsherwood9 <36565536+bsherwood9@users.noreply.github.com>
2019-11-04 22:46:56 -0700

    Met Mvp. Will start on the stretch goals tomorrow.

--- Article/Article.js
+++ Article/Article.js
@@ -3,7 +3,7 @@
 const data = [
   {
     title: 'Lambda School Students: "We\'re the best!"',
-    date: 'Nov 5th, 2018',
+    date: "Nov 5th, 2018",
     firstParagraph: `Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala secura skywalker lando
         moff wicket tatooine luke.Solo wampa wampa calrissian yoda moff.Darth grievous darth gonk darth hutt.Darth baba skywalker
         watto fett jango maul han.Mon ewok sidious sidious lando kenobi grievous gamorrean solo.Yoda wedge utapau darth calamari.
@@ -23,8 +23,8 @@ const data = [
         moff calamari mon obi-wan. Solo grievous lando coruscant. Jinn darth palpatine obi-wan mon.`
   },
   {
-    title: 'Javascript and You, ES6',
-    date: 'May 7th, 2019',
+    title: "Javascript and You, ES6",
+    date: "May 7th, 2019",
     firstParagraph: `Alohamora wand elf parchment, Wingardium Leviosa hippogriff, house dementors betrayal. Holly, Snape centaur portkey ghost
         Hermione spell bezoar Scabbers. Peruvian-Night-Powder werewolf, Dobby pear-tickle half-moon-glasses, Knight-Bus. Padfoot
         snargaluff seeker: Hagrid broomstick mischief managed. Snitch Fluffy rock-cake, 9 ¾ dress robes I must not tell lies. Mudbloods
@@ -43,8 +43,8 @@ const data = [
         sing above the ground, Ginny Weasley bright red. Fanged frisbees, phoenix tears good clean match.`
   },
   {
-    title: 'React vs Angular vs Vue',
-    date: 'June 7th, 2019',
+    title: "React vs Angular vs Vue",
+    date: "June 7th, 2019",
     firstParagraph: `Bulbasaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ivysaur Lorem ipsum dolor sit amet, consectetur adipiscing
         elit. Venusaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charmander Lorem ipsum dolor sit amet, consectetur
         adipiscing elit. Charmeleon Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charizard Lorem ipsum dolor sit amet,
@@ -71,8 +71,8 @@ const data = [
         Castform Lotad the power that's inside Burnt Berry Makuhita. Ghost Ariados Corphish Dusclops Golbat Gligar Zweilous.`
   },
   {
-    title: 'Professional Software Development in 2019',
-    date: 'Jan 1st, 2019',
+    title: "Professional Software Development in 2019",
+    date: "Jan 1st, 2019",
     firstParagraph: `Hodor hodor HODOR! Hodor hodor - hodor, hodor. Hodor hodor... Hodor hodor hodor; hodor hodor. Hodor hodor hodor, hodor, hodor
           hodor. Hodor, hodor. Hodor. Hodor, hodor - hodor... Hodor hodor hodor; hodor HODOR hodor, hodor hodor?! Hodor hodor, hodor.
           Hodor hodor hodor hodor hodor! Hodor hodor - HODOR hodor, hodor hodor hodor hodor hodor; hodor hodor? `,
@@ -85,6 +85,15 @@ const data = [
     thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
           Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
+  },
+  {
+    title: "Brett Sherwood's Improbable Race for the Presidency",
+    date: "Feb 5th, 2029",
+    firstParagraph: `People of the world, Unite! `,
+
+    secondParagraph: `Bacon ipsum dolor amet ut shankle sint eu pork belly. Ea ad duis turkey bresaola. Turducken fugiat dolore ut enim porchetta beef ribs anim aute. Irure in jerky minim do, id spare ribs swine. Ham aliquip jowl, lorem laboris pork loin ea laborum eiusmod meatloaf turkey duis irure filet mignon. Velit tri-tip irure nostrud biltong ham hock beef. Pork loin mollit enim, pork chop esse ut shank sirloin nulla tenderloin exercitation t-bone pork chicken short ribs. `,
+
+    thirdParagraph: `Lorem ipsum dolor amet keffiyeh try-hard butcher air plant kale chips tumblr green juice scenester shaman 3 wolf moon. Messenger bag craft beer mumblecore franzen. Blog la croix cronut brooklyn heirloom gastropub. Listicle sartorial disrupt green juice. Gastropub biodiesel godard tattooed, small batch pinterest etsy.`
   }
 ];
 
@@ -98,17 +107,58 @@ const data = [
 
     <span class='expandButton'></span>
   </div>
-
-  Hint: You will need to use createElement more than once here!
-
-  Your function should take either an object as it's one argument, or 5 separate arguments mapping to each piece of the data object above.
-
-  Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div.
-
-  Step 3: return the entire component.
-
-  Step 4: Map over the data, creating a component for each oject and add each component to the DOM as children of the 'articles' div.
-
-  Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
-
 */
+
+//   Hint: You will need to use createElement more than once here!
+function createArticle(object) {
+  //create elements, set content
+  const article = document.createElement("div");
+  const title = document.createElement("h2");
+  title.textContent = object.title;
+  const pDate = document.createElement("p");
+  pDate.textContent = object.date;
+  const paragraph1 = document.createElement("p");
+  paragraph1.textContent = object.firstParagraph;
+  const paragraph2 = document.createElement("p");
+  paragraph2.textContent = object.secondParagraph;
+  const paragraph3 = document.createElement("p");
+  paragraph3.textContent = object.thirdParagraph;
+  const spanner = document.createElement("span");
+  spanner.textContent = "click here";
+
+  //Append Children
+  article.appendChild(title);
+  article.appendChild(pDate);
+  article.appendChild(paragraph1);
+  article.appendChild(paragraph2);
+  article.appendChild(paragraph3);
+  article.appendChild(spanner);
+
+  //Set Classes
+  article.classList.add("article");
+  pDate.classList.add("date");
+  spanner.classList.add("expandButton");
+
+  //functionality
+  spanner.addEventListener("click", () => {
+    article.classList.toggle("article-open");
+    console.log("clicked");
+  });
+
+  //return it all
+  return article;
+}
+
+//   Your function should take either an object as it's one argument, or 5 separate arguments mapping to each piece of the data object above.
+
+//   Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div.
+
+//   Step 3: return the entire component.
+
+//   Step 4: Map over the data, creating a component for each oject and add each component to the DOM as children of the 'articles' div.
+const parentArticles = document.querySelector(".articles");
+newArticles = data.map(item => {
+  let newArt = createArticle(item);
+  parentArticles.appendChild(newArt);
+});
+//   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,35 +1,54 @@
 /* This is the data we will be using, study it but don't change anything, yet. */
 
 let menuItems = [
-  'Students',
-  'Faculty',
+  "Students",
+  "Faculty",
   "What's New",
-  'Tech Trends',
-  'Music',
-  'Log Out'
+  "Tech Trends",
+  "Music",
+  "Log Out"
 ];
 
-/* 
+// Step 1: Write a function that will create a menu component as seen below:
 
-  Step 1: Write a function that will create a menu component as seen below:
+// <div class="menu">
+//   <ul>
+//     {each menu item as a list item}
+//   </ul>
+// </div>
 
-  <div class="menu">
-    <ul>
-      {each menu item as a list item}
-    </ul>
-  </div>
+// The function takes an array as its only argument.
 
-  The function takes an array as its only argument.
+function newMenu(array) {
+  //create elements
+  const menu = document.createElement("div");
+  const ul = document.createElement("ul");
+  //appending to parent
+  menu.appendChild(ul);
+  //classes
+  menu.classList.add("menu");
 
-  Step 2: Inside this function, iterate over the array creating a list item <li> element for each item in the array. 
-  Add those items to the <ul>
+  array.forEach(item => {
+    const lis = document.createElement("li");
+    lis.textContent = item;
+    ul.appendChild(lis);
+  });
+  const menuImg = document.querySelector(".menu-button");
+  menuImg.addEventListener("click", () => {
+    menu.classList.toggle("menu--open");
+  });
+  return menu;
+}
+const body = document.querySelector("body");
+body.prepend(newMenu(menuItems));
 
-  Step 3: Using a DOM selector, select the menu button (the element with a class of 'menu-button') currently on the DOM.
+// Step 2: Inside this function, iterate over the array creating a list item <li> element for each item in the array.
+// Add those items to the <ul>
 
-  Step 4: add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
+// Step 3: Using a DOM selector, select the menu button (the element with a class of 'menu-button') currently on the DOM.
 
-  Step 5: return the menu component.
+// Step 4: add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
 
-  Step 6: add the menu component to the DOM.
-  
-*/
+// Step 5: return the menu component.
+
+// Step 6: add the menu component to the DOM.
